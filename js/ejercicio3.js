2b4a2ef99bae79d2818d38b2bdb62ca3c2cfdc1e
MauroDS <mauro.diaz.santilli@gmail.com>
2023-03-12 17:28:31 -0300

    Terminando ejercicio 3

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,24 +1,28 @@
 let total = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 let suma = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-function imprimir(){
-  let resultado="";
-  for(let i=2; i<=12; i++){
-    resultado += ("La suma de "+ i +" ocurrió : "+ suma[i] +" veces y resultó en: " + total[i] + "<br/>");
-  }
-  
-  document.write(resultado);
-}
+for(i = 1; i<=50; i++)
+  tirar();
 
-function tirar(){
+ imprimir();
+
+ function tirar(){
   let d1 = Math.floor(1 + Math.random() * 6);
   let d2 = Math.floor(1 + Math.random() * 6);
   suma[d1+d2]++;
   total[d1+d2] += d1+d2;
 }
 
+function imprimir(){
+  let resultado="";
+  for(let i=2; i<=12; i++){
+    resultado += ("El numero "+ i +" ocurrió : "+ suma[i] +" veces y resultó en: " + total[i] + "<br/>");
+  }
+  
+  document.write(resultado);
+}
 
-for(i = 1; i<=50; i++)
-  tirar();
 
-imprimir();
+
+
+
